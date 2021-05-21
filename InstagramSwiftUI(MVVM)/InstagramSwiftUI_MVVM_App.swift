76242c928ef161670c55a6ab7b0879eca67ee44e
Mehdi <sa.mehdisafari@gmail.com>
2021-05-20 19:00:22 -0700

    Initial Commit

---
+++ InstagramSwiftUI(MVVM)/InstagramSwiftUI_MVVM_App.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUI_MVVM_App.swift
+//  InstagramSwiftUI(MVVM)
+//
+//  Created by Mehdi MMS on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUI_MVVM_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
